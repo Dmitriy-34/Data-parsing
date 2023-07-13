8f7dba19c048722b81c6c924a6edd2a56a407a3c
Borisov_Dmitriy <92953878+Dmitriy-34@users.noreply.github.com>
2023-07-14 00:54:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-# import requests  # импортируем модуль
-# import lxml.html
-# from lxml import etree
-#
-# html = requests.get('https://www.python.org/').content  # получим html главной странички официального сайта python
-#
-# tree = lxml.html.document_fromstring(html)
-# title = tree.xpath('/html/head/title/text()')  # забираем текст тега <title> из тега <head>, который лежит
-# # в свою очередь внутри тега <html> (в этом невидимом теге <head> у нас хранится основная информация о страничке,
-# # её название и инструкции по отображению в браузере)
-#
-# print(title)  # выводим полученный заголовок страниц
+import requests  # импортируем модуль
+import lxml.html
+from lxml import etree
+
+html = requests.get('https://www.python.org/').content  # получим html главной странички официального сайта python
+
+tree = lxml.html.document_fromstring(html)
+title = tree.xpath('/html/head/title/text()')  # забираем текст тега <title> из тега <head>, который лежит
+# в свою очередь внутри тега <html> (в этом невидимом теге <head> у нас хранится основная информация о страничке,
+# её название и инструкции по отображению в браузере)
+
+print(title)  # выводим полученный заголовок страниц
 
 
 
 
-# Другой подход парсинга
+# Другой подход в парсинге
 
 import requests  # импортируем модуль
 import lxml.html
